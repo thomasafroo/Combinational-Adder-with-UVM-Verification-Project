09ecec22d6b0f937e66dc9aa37b67f9adb9433ab
thomasafroo <thomasyemane04@gmail.com>
2025-01-11 21:19:03 -0800

    formatting

--- README.md
+++ README.md
@@ -34,15 +34,86 @@ endinterface
 #### Transaction
 
 The `transaction` class represents data items (input and expected output) used during the test.
+```systemverilog
+class transaction extends uvm_sequence_item;
+  rand bit [3:0] a;
+  rand bit [3:0] b;
+  bit [4:0] y;
+  
+    function new(input string path = "transaction");
+      super.new(path);
+    endfunction
+  
+  `uvm_object_utils_begin(transaction)
+    `uvm_field_int(a, UVM_DEFAULT)
+    `uvm_field_int(b, UVM_DEFAULT)
+    `uvm_field_int(y, UVM_DEFAULT)
+  `uvm_object_utils_end
+endclass
 
+```
 #### Generator
 
 The `generator` creates random transactions and sends them to the driver.
-
+```systemverilog
+class generator extends uvm_sequence #(transaction);
+  `uvm_object_utils(generator)
+  
+  transaction t;
+  integer i;
+  
+  function new(input string path = "generator");
+    super.new(path);
+  endfunction
+  
+  
+  virtual task body();
+    t = transaction::type_id::create("t");
+    repeat(10) 
+    begin
+      start_item(t);
+      t.randomize();
+      `uvm_info("GEN",$sformatf("Data send to Driver a :%0d , b :%0d",t.a,t.b), UVM_NONE);
+      finish_item(t);
+    end
+  endtask
+endclass
+```
 #### Driver
 
 The `driver` applies the generated transactions to the DUT using the interface.
 
+```systemverilog
+class driver extends uvm_driver #(transaction);
+  `uvm_component_utils(driver)
+ 
+  function new(input string path = "driver", uvm_component parent = null);
+    super.new(path, parent);
+    endfunction
+ 
+  transaction tc;
+  virtual add_if aif;
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    tc = transaction::type_id::create("tc");
+
+    if(!uvm_config_db #(virtual add_if)::get(this,"","aif",aif)) 
+      `uvm_error("DRV","Unable to access uvm_config_db");
+  endfunction
+ 
+  virtual task run_phase(uvm_phase phase);
+    forever begin
+      seq_item_port.get_next_item(tc);
+      aif.a <= tc.a;
+      aif.b <= tc.b;
+      `uvm_info("DRV", $sformatf("Trigger DUT a: %0d ,b :  %0d",tc.a, tc.b), UVM_NONE); 
+      seq_item_port.item_done();
+      #10;
+    end
+  endtask
+endclass
+```
 #### Monitor
 
 The `monitor` observes the DUT's outputs and sends them to the scoreboard for checking.
--- adder_4_bit_tb.sv
+++ adder_4_bit_tb.sv
@@ -8,80 +8,75 @@
 import uvm_pkg::*;
  
 class transaction extends uvm_sequence_item;
-rand bit [3:0] a;
-rand bit [3:0] b;
-bit [4:0] y;
- 
-  function new(input string path = "transaction");
-    super.new(path);
-  endfunction
- 
-`uvm_object_utils_begin(transaction)
-  `uvm_field_int(a, UVM_DEFAULT)
-  `uvm_field_int(b, UVM_DEFAULT)
-  `uvm_field_int(y, UVM_DEFAULT)
-`uvm_object_utils_end
- 
+  rand bit [3:0] a;
+  rand bit [3:0] b;
+  bit [4:0] y;
+  
+    function new(input string path = "transaction");
+      super.new(path);
+    endfunction
+  
+  `uvm_object_utils_begin(transaction)
+    `uvm_field_int(a, UVM_DEFAULT)
+    `uvm_field_int(b, UVM_DEFAULT)
+    `uvm_field_int(y, UVM_DEFAULT)
+  `uvm_object_utils_end
 endclass
  
 //////////////////////////////////////////////////////////////
 class generator extends uvm_sequence #(transaction);
-`uvm_object_utils(generator)
- 
-transaction t;
-integer i;
- 
+  `uvm_object_utils(generator)
+  
+  transaction t;
+  integer i;
+  
   function new(input string path = "generator");
     super.new(path);
   endfunction
- 
- 
-virtual task body();
-  t = transaction::type_id::create("t");
-  repeat(10) 
+  
+  
+  virtual task body();
+    t = transaction::type_id::create("t");
+    repeat(10) 
     begin
-    start_item(t);
-    t.randomize();
-    `uvm_info("GEN",$sformatf("Data send to Driver a :%0d , b :%0d",t.a,t.b), UVM_NONE);
-    finish_item(t);
+      start_item(t);
+      t.randomize();
+      `uvm_info("GEN",$sformatf("Data send to Driver a :%0d , b :%0d",t.a,t.b), UVM_NONE);
+      finish_item(t);
     end
-endtask
- 
+  endtask
 endclass
  
 ////////////////////////////////////////////////////////////////////
  
 class driver extends uvm_driver #(transaction);
-`uvm_component_utils(driver)
+  `uvm_component_utils(driver)
  
-    function new(input string path = "driver", uvm_component parent = null);
-      super.new(path, parent);
-     endfunction
- 
-transaction tc;
-virtual add_if aif;
- 
- 
-    virtual function void build_phase(uvm_phase phase);
-      super.build_phase(phase);
-      tc = transaction::type_id::create("tc");
- 
-      if(!uvm_config_db #(virtual add_if)::get(this,"","aif",aif)) 
-      `uvm_error("DRV","Unable to access uvm_config_db");
+  function new(input string path = "driver", uvm_component parent = null);
+    super.new(path, parent);
     endfunction
  
-    virtual task run_phase(uvm_phase phase);
+  transaction tc;
+  virtual add_if aif;
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    tc = transaction::type_id::create("tc");
+
+    if(!uvm_config_db #(virtual add_if)::get(this,"","aif",aif)) 
+      `uvm_error("DRV","Unable to access uvm_config_db");
+  endfunction
+ 
+  virtual task run_phase(uvm_phase phase);
     forever begin
-      
-    seq_item_port.get_next_item(tc);
-    aif.a <= tc.a;
-    aif.b <= tc.b;
+      seq_item_port.get_next_item(tc);
+      aif.a <= tc.a;
+      aif.b <= tc.b;
       `uvm_info("DRV", $sformatf("Trigger DUT a: %0d ,b :  %0d",tc.a, tc.b), UVM_NONE); 
-    seq_item_port.item_done();
-    #10;  
-      
+      seq_item_port.item_done();
+      #10;
     end
-    endtask
+  endtask
 endclass
  
 ////////////////////////////////////////////////////////////////////////
